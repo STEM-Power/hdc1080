6838330cddfb00b6e4145e9ac1aaecf3e0c44d21
STEM-Power <81220298+STEM-Power@users.noreply.github.com>
2021-04-05 16:53:19 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -20,10 +20,10 @@ namespace HDC1080 {
     export function HDC1080_init(): void {
         pins.i2cWriteNumber(HDC1080_I2C_ADDR, 0x02, 0x0000); //*config the HDC1080 to 14 bits TH and 14 bits RH measure separately, heater disabled//
         pins.i2cWriteNumber(HDC1080_I2C_ADDR, 0x00, NumberFormat.UInt8BE, false); //*Temperature measurement//
-        basic.pause(10)
+        basic.pause(10);
         pins.i2cReadNumber(HDC1080_I2C_ADDR, NumberFormat.UInt16BE);     
         pins.i2cWriteNumber(HDC1080_I2C_ADDR, 0x01, NumberFormat.UInt8BE, false);   //*Humidity measurement//
-        basic.pause(10)
+        basic.pause(10);
         pins.i2cReadNumber(HDC1080_I2C_ADDR, NumberFormat.UInt16BE);
         
     }
@@ -34,7 +34,7 @@ namespace HDC1080 {
     //% blockId="read_device_info" block="read device info by command"
     //% weight=99 blockGap=8
     export function read_deviceID(): number {
-        pins.i2cWriteNumber(HDC1080_I2C_ADDR, 0xFF, NumberFormat.UInt8BE)
+        pins.i2cWriteNumber(HDC1080_I2C_ADDR, 0xFF, NumberFormat.UInt8BE);
         return pins.i2cReadNumber(HDC1080_I2C_ADDR, NumberFormat.UInt16BE);
     }
 
@@ -44,7 +44,8 @@ namespace HDC1080 {
     //% blockId="read_temperture" block="Read Temperature"
     //% weight=90 blockGap=8
     export function read_temperature(): number {
-        pins.i2cWriteNumber(HDC1080_I2C_ADDR, 0x00, NumberFormat.UInt8BE)
+        pins.i2cWriteNumber(HDC1080_I2C_ADDR, 0x00, NumberFormat.UInt8BE);
+        basic.pause(10);
         return pins.i2cReadNumber(HDC1080_I2C_ADDR, NumberFormat.UInt16BE);
     }
 
