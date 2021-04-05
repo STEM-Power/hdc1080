83724694cedfd6e51660eca905477c7c555cfd78
STEM-Power <81220298+STEM-Power@users.noreply.github.com>
2021-04-05 17:43:15 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -38,7 +38,8 @@ namespace HDC1080 {
         pins.i2cWriteNumber(HDC1080_I2C_ADDR, 0x00, NumberFormat.UInt8BE, false);
         basic.pause(10);
         let TH_raw = pins.i2cReadNumber(HDC1080_I2C_ADDR, NumberFormat.UInt16BE);
-        let TH = TH_raw / 65536 * 165 - 40
+        let TH_buf = TH_raw / 65536 
+        let TH = Math.roundWithPrecision(TH_buf, 2) * 165 - 40
         return Math.roundWithPrecision(TH, 1);
     }
 
